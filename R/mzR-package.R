5efe3b3994c520dcfb515661d67fceef3f9b7326
Gregor de Cillia <de.cillia.gregor@gmail.com>
2018-03-21 09:22:15 +0100

    Add help page for package `?mzR`

---
+++ R/mzR-package.R
@@ -0,0 +1,26 @@
+#' mzR: Microcensus tools
+#'
+#' R package to compute estimates and their errors for Austrian Microcensus (labour force survey) data.
+#' 
+#' @section Testdaten:
+#' [mzTestData]
+#' 
+#' @section Import von STAT internen Daten:
+#' [ImportData], [ImportDataListQT]
+#' 
+#' @section Individueller Datenimport:
+#' [IndivImportData]
+#' 
+#' @section Schaetz- und Fehlerrechnungsfunktionen:
+#' [GroupRate], [GroupSize], [Mean], [Total]
+#' 
+#' @section Excel export:
+#' [MakeTable], [MakeQT], [FillExcelTemplate]
+#' 
+#' @section Utility Funktionen:
+#' [AddVariable], [GetLabels]
+#'
+#' @md
+#' @name mzR
+#' @docType package
+NULL
