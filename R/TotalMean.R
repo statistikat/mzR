5354f1660cce458c3892c082eddeb3bfe97cdafc
Angelika Meraner - B <meraner@xlwt0006.statistik.local>
2018-11-19 15:14:57 +0100

    Fix von Quick Fix (beide in einem File)

--- R/TotalMean.R
+++ R/TotalMean.R
@@ -86,7 +86,7 @@ MeanX <- function(xx, TFstring, var, negativeZero = TRUE, replicates = replicate
   
   x[,gew1Num:=gew1*varNumTmp]
   x_bw <- copy(x)
-  x[, (bw) := lapply(.SD, function(y){y * x[['varNumTmp']]}), .SDcols = bw]
+  x[, (bw) := lapply(.SD, function(y){y * x_bw[['varNumTmp']]}), .SDcols = bw]
   setnames(x,old=bw,new=bwNum)
   
   if(length(xx)>1){
@@ -101,7 +101,7 @@ MeanX <- function(xx, TFstring, var, negativeZero = TRUE, replicates = replicate
     
     y[,gew1Num:=gew1*varNumTmp]
     y_bw <- copy(y)
-    y[, (bw) := lapply(.SD, function(z){z * y[['varNumTmp']]}), .SDcols = bw]
+    y[, (bw) := lapply(.SD, function(z){z * y_bw[['varNumTmp']]}), .SDcols = bw]
     setnames(y,old=bw,new=bwNum)
   }
   num <- x[eval(parse(text=TFstring)),sum(gew1Num)]
